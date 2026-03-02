b2d0bf383b8fe68f3d74eb261571b4d92709d246
pedroren <pedroren@gmail.com>
2026-03-02 13:41:52 -0800

    draft

---
+++ macropad/ergogen/runergogen.sh
@@ -0,0 +1,4 @@
+ergogen .
+openjscad output/cases/case.jscad -of stla -o cases/case.stl
+openjscad output/cases/switch_plate.jscad -of stla -o cases/switch_plate.stl
+mv output/pcbs/*.kicad_pcb ./pcbs/
