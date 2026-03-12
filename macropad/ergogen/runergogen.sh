add14717742d7751e26b59c1cc74aa78113446b9
pedroren <pedroren@gmail.com>
2026-03-11 19:43:20 -0700

    More fixes to the case/pcb/plate

--- macropad/ergogen/runergogen.sh
+++ macropad/ergogen/runergogen.sh
@@ -1,6 +1,7 @@
 ergogen .
 openjscad output/cases/case.jscad -of stla -o cases/case.stl
 openjscad output/cases/switch_plate.jscad -of stla -o cases/switch_plate.stl
+openjscad output/cases/switch_plate.jscad -of stla -o cases/usb_access_lip.stl
 openjscad output/cases/case_plate_preview.jscad -of stla 
 openjscad output/cases/case_pcb_preview.jscad -of stla 
 mv output/pcbs/*.kicad_pcb ./pcbs/
