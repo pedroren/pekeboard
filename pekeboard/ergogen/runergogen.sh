2686879401af5ff623c7b5d23af3568d8a62ba75
pedroren <pedroren@gmail.com>
2026-04-06 12:54:53 -0700

    Sandwich style bottom case

--- pekeboard/ergogen/runergogen.sh
+++ pekeboard/ergogen/runergogen.sh
@@ -2,7 +2,8 @@ ergogen .
 openjscad output/cases/case.jscad -of stla -o cases/case.stl
 #openjscad output/cases/pcb.jscad -of stla -o cases/pcb.stl
 openjscad output/cases/switch_plate.jscad -of stla -o cases/switch_plate.stl
-#openjscad output/cases/usb_access_lip.jscad -of stla -o cases/usb_access_lip.stl
+openjscad output/cases/sandwich_case.jscad -of stla -o cases/sandwich_case.stl
+
 openjscad output/cases/case_plate_preview.jscad -of stla 
 openjscad output/cases/case_pcb_preview.jscad -of stla 
 mv output/pcbs/*.kicad_pcb ./pcbs/
