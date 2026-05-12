d59d64c9afdbbbfcde16ac9bec5741e9f32d7507
pedroren <pedroren@gmail.com>
2026-05-11 18:34:57 -0700

    Removing pcb mounting hole, only screw Adding plate support o-rings

--- pekeboard/runergogen.sh
+++ pekeboard/runergogen.sh
@@ -4,6 +4,10 @@ openjscad output/cases/case.jscad -of stla -o cases/case.stl
 #openjscad output/cases/pcb.jscad -of stla -o cases/pcb.stl
 openjscad output/cases/switch_plate.jscad -of stla -o cases/switch_plate.stl
 openjscad output/cases/sandwich_case.jscad -of stla -o cases/sandwich_case.stl
+openjscad output/cases/plate_support.jscad -of stla -o cases/plate_support.stl
 
 openjscad output/cases/case_plate_preview.jscad -of stla 
 openjscad output/cases/case_pcb_preview.jscad -of stla 
+
+cd pcbs
+#java -jar freerouting-current.jar -de pepekey.dsn -do pepekey.ses -da -dct 1
