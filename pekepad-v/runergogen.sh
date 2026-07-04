c3fd333f14c27eb6d11d5754a56ae2cc69fad54c
pedroren <pedroren@gmail.com>
2026-07-04 13:21:08 -0700

    pekepad-v top_gasket

--- pekepad-v/runergogen.sh
+++ pekepad-v/runergogen.sh
@@ -4,6 +4,7 @@ openjscad output/cases/case.jscad -of stla -o cases/case.stl
 #openjscad output/cases/pcb.jscad -of stla -o cases/pcb.stl
 openjscad output/cases/switch_plate.jscad -of stla -o cases/switch_plate.stl
 openjscad output/cases/sandwich_case.jscad -of stla -o cases/sandwich_case.stl
+openjscad output/cases/top_gasket.jscad -of stla -o cases/top_gasket.stl
 
 #openjscad output/cases/case_plate_preview.jscad -of stla 
 #openjscad output/cases/case_pcb_preview.jscad -of stla 
